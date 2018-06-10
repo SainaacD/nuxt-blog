49c5a23d041f32e0c63e9dcba846f99db2d13f39
Sainaac <&06388859@qq.com>
2018-06-10 20:02:12 +0800

    added action bar”

--- pages/tags.vue
+++ pages/tags.vue
@@ -1,15 +1,51 @@
 <template>
   <div class="tags">
+    <Action :data="action" :choose="choose"/>
   </div>
 </template>
 
 <script>
 
+import Action from '~/components/Action.vue'
+
 export default {
   components: {
+    Action
+  },
+  computed: {
+    choose() {
+      return this.$store.state.action.choose
+    }
   },
   data() {
     return {
+      action: [
+        {
+          iconfont: true,
+          content: 'home',
+          link: 'index'
+        },
+        {
+          iconfont: true,
+          content: 'tags',
+          link: 'tags'
+        },
+        {
+          iconfont: true,
+          content: 'plus icon-special',
+          link: 'add'
+        },
+        {
+          iconfont: true,
+          content: 'search',
+          link: 'search'
+        },
+        {
+          iconfont: true,
+          content: 'user',
+          link: 'user'
+        }
+      ]
     }
   },
   methods: {
